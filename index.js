cf4dce42241ad8195151eeea55f47264ebe39c5f
farahkhan678 <441farahkhan@gmail.com>
2023-08-25 07:37:27 +0530

    first commit

---
+++ index.js
@@ -0,0 +1,10 @@
+var radiobtn = document.getElementById("radiobtn").defaultChecked;
+var selId = document.getElementById("size");
+selId.value = "4";
+
+var selId = document.getElementById("colour");
+selId.value = "4";
+var selId = document.getElementById("2size");
+selId.value = "4";
+var selId = document.getElementById("2colour");
+selId.value = "1";
